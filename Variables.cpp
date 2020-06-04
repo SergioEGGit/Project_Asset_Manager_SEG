d52f1212172cc367eb6a82ee3b34d8d4b04148a2
SergioEGGit <sergioeg19@hotmail.com>
2020-06-04 15:26:36 -0500

    Primer Commit E Interfaz

---
+++ Variables.cpp
@@ -0,0 +1,23 @@
+//------------------------------------------------------------------------------
+
+	#pragma hdrstop
+
+//-----------------------------Librerias----------------------------------------
+
+	#include "Variables.h"
+	#include<iostream>
+	#include<string>
+
+	using namespace std;
+
+//-----------------------------Variables----------------------------------------
+
+	string Variables::UsuarioA = "";
+	string Variables::DepartamentoUA = "";
+	string Variables::EmpresaUA = "";
+
+//------------------------------------------------------------------------------
+
+	#pragma package(smart_init)
+
+
---
+++ Variables.h
@@ -0,0 +1,26 @@
+//------------------------------------------------------------------------------
+
+	#ifndef VariablesH
+	#define VariablesH
+
+//-----------------------------Librerias----------------------------------------
+
+	#include<iostream>
+	#include<string>
+
+	using namespace std;
+
+//-----------------------------Variables----------------------------------------
+
+	namespace Variables
+	{
+		extern string UsuarioA;
+		extern string DepartamentoUA;
+		extern string EmpresaUA;
+	}
+
+//------------------------------------------------------------------------------
+
+	#endif
+
+
