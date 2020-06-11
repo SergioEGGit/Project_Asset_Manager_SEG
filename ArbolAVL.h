bd0da57f7a560d72f1f9e0355b9ff9d614cdc571
SergioEGGit <sergioeg19@hotmail.com>
2020-06-10 21:48:58 -0500

    Grafica Matriz Y Comienzo AVL

---
+++ ArbolAVL.h
@@ -0,0 +1,35 @@
+//------------------------------------------------------------------------------
+
+	#ifndef ArbolAVLH
+	#define ArbolAVLH
+
+//-----------------------------Librerias----------------------------------------
+
+	#include <iostream>
+	#include <string>
+	#include <cstring>
+
+	using namespace std;
+
+//-----------------------------Estructura---------------------------------------
+
+	struct ArbolAVLEstructura
+	{
+		bool ExisteRenta;
+		int Identificador;
+		int AlturaNodo;
+		string CodigoAlfaNumerico;
+		string NombreActivo;
+		string DescripcionActivo;
+		ArbolAVLEstructura *RI;
+		ArbolAVLEstructura *LE;
+	};
+
+	typedef struct ArbolAVLEstructura *ArbolAVLActivos;
+
+//--------------------------------Métodos---------------------------------------
+
+
+
+//---------------------------------------------------------------------------
+	#endif
